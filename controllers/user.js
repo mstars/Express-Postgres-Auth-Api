dd569811fadf0d9d2037881d291c4c329c2c81e6
mstars <vellackallintothomas@gmail.com>
2020-03-28 11:04:58 +0530

    added user model and controller

---
+++ controllers/user.js
@@ -0,0 +1,28 @@
+const { pool } = require('./../models/config')
+
+class user {
+  constructor() {
+    const login = (request, response) => {
+      pool.query('SELECT * FROM auth_tab', (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).json(results.rows)
+      })
+    }
+
+    const createAccount = (request, response) => {
+      const { author, title } = request.body
+
+      pool.query('INSERT INTO auth_tab (uname, password) VALUES ($1, $2)', [uname, password], error => {
+        if (error) {
+          throw error
+        }
+        response.status(201).json({ status: 'success', message: 'Registered succesfully.' })
+      })
+    }
+  }
+}
+
+
+module.exports = new user();
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const { pool } = require('./config')
+const { pool } = require('./models/config')
+const { userController } = require('./controllers/user')
+const router = express.Router();
 
 const app = express()
 
@@ -9,10 +11,26 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
 // when a random route is inputed
-app.get('*', (req, res) => res.status(200).send({
-   message: 'Server is running.'
+app.get('/', (req, res) => res.status(200).send({
+   appName        :'Postgres authentication server with express.',
+   serverStatus   :'Server is running.',
+   lastUpdatedTime:dateTime,
+   auther         :'LT'
 }));
+
+
+  app.route('/auth')
+    // GET endpoint
+    router.get(userController.login)
+    // POST endpoint
+    router.post(userController.createAccount)
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening in http://localhost:3000`)

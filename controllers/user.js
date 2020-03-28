9f373091faadaff58821cb9e251f6eeb19cf83a5
mstars <vellackallintothomas@gmail.com>
2020-03-28 19:28:33 +0530

    login api initialted.

--- controllers/user.js
+++ controllers/user.js
@@ -2,19 +2,23 @@ const { pool } = require('./../models/dbconnection');
 
 class userController {
     async doLogin (request, response){
-      pool.query('SELECT * FROM auth_tab', (error, results) => {
+      const { uname, password } = request.body
+      pool.query("SELECT * FROM auth_tab WHERE uname= $1",[uname], (error, results) => {
         if (error) {
           throw error
         }
         response.status(200).json(results.rows)
+        response.status(201).json({ status: 'success', message: 'Login succesfull.' })
       })
     }
 
     async doCreateAccount (request, response){
       const { uname, password } = request.body
-      pool.query('INSERT INTO auth_tab (uname, password) VALUES ($1, $2)', [uname, password], error => {
+      console.log(uname);
+      pool.query("INSERT INTO auth_tab (uname, password) VALUES ($1, $2)", [uname, password], error => {
         if (error) {
-          throw error
+          // throw error
+          response.status(201).json({ status: 'faield', message: 'Registered unsuccesfully.' })
         }
         response.status(201).json({ status: 'success', message: 'Registered succesfully.' })
       })

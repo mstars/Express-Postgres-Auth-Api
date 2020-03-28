aa9a17933369623aaec2f81997ed0a9e8ada2955
mstars <vellackallintothomas@gmail.com>
2020-03-28 14:41:32 +0530

    router changes.

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController  = require('./../controllers/user');
 
-router.post('/v1/users/createAccount',userController.createAccount);
-router.get('/v1/users/view',userController.view);
+router.post('/v1/users/createAccount',userController.doCreateAccount);
+router.post('/v1/users/login',userController.doLogin);
 
 
 module.exports = router;

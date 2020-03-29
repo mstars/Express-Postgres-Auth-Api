e2524b8b856cfb7f77d4f6f278e4d1979c7c8525
mstars <vellackallintothomas@gmail.com>
2020-03-29 14:08:06 +0530

    added a table creation.

---
+++ models/db.js
@@ -0,0 +1,34 @@
+const { pool } = require('./../models/dbconnection');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+/**
+ * Create Reflection Table
+ */
+const createUserTable = () => {
+  const queryText =
+    `CREATE TABLE IF NOT EXISTS
+      auth_tab(
+        uid SERIAL PRIMARY KEY,
+        uname VARCHAR(25) UNIQUE NOT NULL,
+        password VARCHAR(25) UNIQUE NOT NULL,
+      )`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+module.exports = {
+  createUserTable
+};
+
+require('make-runnable');

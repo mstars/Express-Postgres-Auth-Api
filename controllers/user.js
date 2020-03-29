30037c1dbc2dfad745da3de50416bdaaee2616a1
mahi <mahesh30795@gmail.com>
2020-03-29 17:06:43 +0530

    new way for client

--- controllers/user.js
+++ controllers/user.js
@@ -3,24 +3,23 @@ const { pool } = require('./../models/dbconnection');
 class userController {
   async doLogin(request, response) {
     const { uname, password } = request.body
+    const client = await pool.connect().catch(err => {
+      console.log(err);
+    })
     try {
-      pool.connect().then(client => {
-        client.query("SELECT * FROM auth_tab WHERE uname= $1", [uname], (error, results) => {
-          if (error) {
-            client.release();
-            return response.status(201).json({ status: 'failed', message: 'Login unsuccesful.', error })
-          }
-          if (results.rows[0].uname == uname && results.rows[0].password == password) {
-            client.release();
-            return response.status(201).json({ status: 'sucess', message: 'Login succesfully.' })
-          }
-          else {
-            client.release();
-            return response.status(201).json({ status: 'failed', message: 'Login unsuccesfully.' })
-          }
-        })
-      }).catch(err=>{
-        console.log(err);
+      client.query("SELECT * FROM auth_tab WHERE uname= $1", [uname], (error, results) => {
+        if (error) {
+          client.release();
+          return response.status(201).json({ status: 'failed', message: 'Login unsuccesful.', error })
+        }
+        if (results.rows[0].uname == uname && results.rows[0].password == password) {
+          client.release();
+          return response.status(201).json({ status: 'sucess', message: 'Login succesfully.' })
+        }
+        else {
+          client.release();
+          return response.status(201).json({ status: 'failed', message: 'Login unsuccesfully.' })
+        }
       })
     }
     catch (err) {
@@ -37,28 +36,24 @@ class userController {
 
   async doCreateAccount(request, response) {
     const { uname, password } = request.body
+    const client = await pool.connect().catch(err => {
+      console.log(err);
+    })
     try {
-      pool.connect().then(client => {
-        client.query("INSERT INTO auth_tab (uname, password) VALUES ($1, $2)", [uname, password], error => {
-          if (error) {
-            // throw error
-            if (error.code == '23505') {
-              client.release();
-              return response.status(400).json({ status: 'failed', message: 'Registered unsuccesfully. Reason: Username already taken.' })
-            }
-            if (error.code == '23502') {
-              client.release();
-              return response.status(400).json({ status: 'failed', message: 'Registered unsuccesfully. Reason: Username can not be null.' })
-            }
-            client.release();
-            return response.status(201).json({ status: 'failed', message: 'Registered unsuccesfully.', error })
+      client.query("INSERT INTO auth_tab (uname, password) VALUES ($1, $2)", [uname, password], error => {
+        if (error) {
+          // throw error
+          if (error.code == '23505') {
+            return response.status(400).json({ status: 'failed', message: 'Registered unsuccesfully. Reason: Username already taken.' })
           }
-          client.release();
-          return response.status(201).json({ status: 'success', message: 'Registered succesfully.' })
-        })
-      }).catch(err=>{
-        console.log(err);
+          if (error.code == '23502') {
+            return response.status(400).json({ status: 'failed', message: 'Registered unsuccesfully. Reason: Username can not be null.' })
+          }
+          return response.status(201).json({ status: 'failed', message: 'Registered unsuccesfully.', error })
+        }
+        return response.status(201).json({ status: 'success', message: 'Registered succesfully.' })
       })
+
     }
     catch (err) {
       return response.status(400).send({

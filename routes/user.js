6fe7120f380555f23aad0fae677745f18ba092f5
mstars <vellackallintothomas@gmail.com>
2020-03-30 19:36:11 +0530

    minor fix on routes in mail confirmation.

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const userCreationController  = require('./../controllers/User/createAccount');
 const userLoginController  = require('./../controllers/User/login');
-const userLoginVerificationController  = require('./../controllers/User/loginMailVerification');
+const userMailVerificationController  = require('./../controllers/User/loginMailVerification');
 
 router.post('/v1/users/createAccount',userCreationController.doCreateAccount);
 router.post('/v1/users/login',userLoginController.doLogin);
-router.get('/v1/users/verify',userLoginVerificationController.verifyToken);
+router.get('/v1/users/verify',userMailVerificationController.verifyToken);
 
 module.exports = router;
